20c8411afc4725c1cecc54d962e289ff6e04f833
hamza-khalid-qa <hamzakhalidqae@gmail.com>
2025-03-10 16:26:14 +0500

    first commit to main branch

---
+++ env.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+final class _Env {
+  static const String serpapiApiKey =
+      '73e72e631494487393bec64089ac371ea7ba15e9a50f3c1ba840d84233666f77';
+}
