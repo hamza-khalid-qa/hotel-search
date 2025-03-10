1fc05567bf50a9e7dd1ce14d10077a7cee56ec0b
hamza-khalid-qa <hamzakhalidqae@gmail.com>
2025-03-10 17:30:23 +0500

    commit check

---
+++ integration_test/overview_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hotel_booking/main.dart';
+import 'package:hotel_booking/features/overview/presentation/pages/overview_page.dart';
+//overview test
+void main() {
+  testWidgets('OverviewPage displays the correct app bar title', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: OverviewPage(),
+      ),
+    );
+
+    expect(find.text('Hotel Booking'), findsOneWidget);
+  });
+}
